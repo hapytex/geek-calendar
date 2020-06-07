a39db206e970f524381da5fa927e3214671b7c05
Willem Van Onsem <vanonsem.willem@gmail.com>
2020-06-07 15:42:48 +0200

    start working on rendering part

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,7 @@
 module Main where
 
+months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
 main :: IO ()
 main = do
   putStrLn "hello world"
