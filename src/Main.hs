3007e426fcc2115555acdd1b39b80f29bbab0541
Willem Van Onsem <vanonsem.willem@gmail.com>
2020-08-28 22:16:20 +0200

    generate first iCal stream

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,20 @@
 module Main where
 
+import qualified Data.ByteString.Lazy as B
+import Data.Default(def)
+import Data.Time.Clock(getCurrentTime)
+
 import Geek.Data
 import Geek.Parsing(parseBirthdays)
+
+import Text.ICalendar.Printer(printICalendar)
 
 months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
 main :: IO ()
 main = do
   l <- parseBirthdays
-  print l
+  t <- getCurrentTime
+  let c = packCalendar t l
+  B.putStr (printICalendar def c)
+  putStrLn ""
